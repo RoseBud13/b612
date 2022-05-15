e3eb81456f0f402e6249b7a641589b42c308f4f2
RoseBud13 <xiongkaijie13@gmail.com>
2022-05-16 06:03:46 +0800

    add book and film shelf template

--- client/src/components/Dashboard.vue
+++ client/src/components/Dashboard.vue
@@ -98,7 +98,73 @@
                             </div>
                         </li>
                         <li class="app-window-slide-item">
-                            books and movies
+                            <div class="shelf-view">
+                                <div class="shelf-items">
+                                    <div class="shelf-upper">
+                                        <div class="book-item">
+                                            <img src="../assets/img/book_cover.jpeg" alt="book">
+                                        </div>
+                                        <div class="book-item">
+                                            <img src="../assets/img/book_cover.jpeg" alt="book">
+                                        </div>
+                                        <div class="book-item">
+                                            <img src="../assets/img/book_cover.jpeg" alt="book">
+                                        </div>
+                                        <div class="book-item">
+                                            <img src="../assets/img/book_cover.jpeg" alt="book">
+                                        </div>
+                                        <div class="book-item">
+                                            <img src="../assets/img/book_cover.jpeg" alt="book">
+                                        </div>
+                                        <div class="book-item">
+                                            <img src="../assets/img/book_cover.jpeg" alt="book">
+                                        </div>
+                                        <div class="book-item">
+                                            <img src="../assets/img/book_cover.jpeg" alt="book">
+                                        </div>
+                                        <div class="book-item">
+                                            <img src="../assets/img/book_cover.jpeg" alt="book">
+                                        </div>
+                                    </div>
+                                    <div class="shelf-lower">
+                                        <div class="film-item">
+                                            <img src="http://image.tmdb.org/t/p/w500//aKCvdFFF5n80P2VdS7d8YBwbCjh.jpg" alt="film">
+                                        </div>
+                                        <div class="film-item">
+                                            <img src="http://image.tmdb.org/t/p/w500//aKCvdFFF5n80P2VdS7d8YBwbCjh.jpg" alt="film">
+                                        </div>
+                                        <div class="film-item">
+                                            <img src="http://image.tmdb.org/t/p/w500//aKCvdFFF5n80P2VdS7d8YBwbCjh.jpg" alt="film">
+                                        </div>
+                                        <div class="film-item">
+                                            <img src="http://image.tmdb.org/t/p/w500//aKCvdFFF5n80P2VdS7d8YBwbCjh.jpg" alt="film">
+                                        </div>
+                                        <div class="film-item">
+                                            <img src="http://image.tmdb.org/t/p/w500//aKCvdFFF5n80P2VdS7d8YBwbCjh.jpg" alt="film">
+                                        </div>
+                                        <div class="film-item">
+                                            <img src="http://image.tmdb.org/t/p/w500//aKCvdFFF5n80P2VdS7d8YBwbCjh.jpg" alt="film">
+                                        </div>
+                                        <div class="film-item">
+                                            <img src="http://image.tmdb.org/t/p/w500//aKCvdFFF5n80P2VdS7d8YBwbCjh.jpg" alt="film">
+                                        </div>
+                                        <div class="film-item">
+                                            <img src="http://image.tmdb.org/t/p/w500//aKCvdFFF5n80P2VdS7d8YBwbCjh.jpg" alt="film">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="shelf-item-detail">
+                                    <div class="item-wrapper">
+                                        <img src="../assets/img/book_cover.jpeg" alt="book">
+                                        <div class="item-title">
+                                            <p>The Perks of Being a Wallflower</p>
+                                        </div>
+                                        <div class="item-content">
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore vitae, iusto sed explicabo dolores consequatur pariatur, rerum tempora illo provident ad necessitatibus consectetur atque culpa itaque maxime delectus iste corrupti.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </li>
                         <li class="app-window-slide-item">
                             to-do list
@@ -234,7 +300,7 @@ export default defineComponent({
     width: 100%;
     // height: calc(100% - 50px);
     height: 100%;
-    color: rgb(255, 255, 255);
+    // color: rgb(255, 255, 255);
     // margin-top: 50px;
     display: flex;
 
@@ -370,7 +436,8 @@ export default defineComponent({
     position: absolute;
     top: 0;
     left: 0;
-    overflow: auto;
+    overflow-y: auto;
+    overflow-x: hidden;
 
     transition: .5s transform ease-in-out;
 }
@@ -418,6 +485,113 @@ input[type="radio"] {
     height: auto;
     border-radius: 10px;
 }
+.shelf-view {
+    display: flex;
+    // flex-direction: column;
+    width: 100%;
+    height: 100%;
+
+    // * {
+    //     border: 1px solid black;
+    //     background-color: #eee;
+    // }
+}
+.shelf-items {
+    flex: 1;
+
+    display: flex;
+    flex-direction: column;
+    max-width: calc(100% - 320px);
+    height: 100%;
+}
+.shelf-item-detail {
+    min-width: 320px;
+    width: 320px;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.shelf-upper {
+    flex: 1;
+
+    display: flex;
+    align-items: center;
+    overflow-x: auto;
+    height: 100%;
+}
+.shelf-upper::-webkit-scrollbar {
+    display: none;
+}
+.book-item {
+    height: 60%;
+    aspect-ratio: 6 / 7;
+    margin: 0 0 0 30px;
+    background-color: rgba(137, 210, 235, 0.7);
+    border-radius: 13%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.book-item img {
+    height: 70%;
+    border-radius: 13%;
+}
+.shelf-lower {
+    flex: 1;
+
+    display: flex;
+    align-items: center;
+    overflow-x: auto;
+    height: 100%;
+}
+.shelf-lower::-webkit-scrollbar {
+    display: none;
+}
+.film-item {
+    height: 60%;
+    aspect-ratio: 6 / 7;
+    margin: 0 0 0 30px;
+    background-color: rgba(170, 235, 137, 0.7);
+    border-radius: 13%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.film-item img {
+    height: 70%;
+    border-radius: 13%;
+}
+.item-wrapper {
+    height: 90%;
+    width: 100%;
+    background-color: rgba(137, 210, 235, 1);
+    text-align: center;
+    border-radius: 20px;
+}
+.item-wrapper img {
+    height: 40%;
+    margin: 10px;
+}
+.item-title {
+    margin-top: 20px;
+}
+.item-title p {
+    font-size: 1.1rem;
+    color: #fff;
+}
+.item-content {
+    margin: 20px 15px;
+    padding: 10px;
+    text-align: justify;
+}
+.item-content p {
+    font-size: 0.9rem;
+    color: #fff;
+}
+
 .apps {
     flex: 2;
 
@@ -570,6 +744,20 @@ input[type="radio"] {
     }
     .icon-container {
         margin: 15px;
+    }
+    .shelf-items {
+        max-width: calc(100% - 280px);
+    }
+    .shelf-item-detail {
+        min-width: 280px;
+        width: 280px;
+    }
+    .item-wrapper img {
+        height: 35%;
+        margin: 10px;
+    }
+    .item-title {
+        margin-top: 10px;
     }
 }
 @media (max-width: 820px) {
